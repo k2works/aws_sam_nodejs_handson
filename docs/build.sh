5e4d5a9833bb4c7f498df8a6353ce6d646b27e03
k2works <kakimomokuri@gmail.com>
2018-08-11 16:23:20 +0900

    ドキュメントセットアップ

--- docs/build.sh
+++ docs/build.sh
@@ -3,3 +3,4 @@
 cd docs
 rm -rf dist
 bundle exec asciidoctor -r asciidoctor-diagram src/index.adoc -D ./dist
+bundle exec asciidoctor -r asciidoctor-diagram src/fizz_buzz/*.adoc -D ./dist/fizz_buzz
